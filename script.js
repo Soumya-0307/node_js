6e0291e478c7a8134142d8c6e34cd41923d72636
soumya Hegde <soumyahegde652@gmail.com>
2023-12-13 00:09:13 +0530

    node basics with process and process.argv

--- script.js
+++ script.js
@@ -9,5 +9,9 @@ for (let i=0; i < n; i++) {
 args=process.argv;
 
 for (let i=2;i<args.length;i++){
-    console.log("hello to",args[i]);
+    // console.log("hello to",args[i]);
 }
+
+const somevalue=require("./math");
+
+console.log(somevalue);
